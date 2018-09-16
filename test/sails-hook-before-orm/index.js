db72a56653b976426f6e7e71d9301f772ce72336
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-16 15:53:25 +0200

    fix(Hook): fix mistaken hook dir
    generally when a module requires another module, Node caches the read file. When the same module is
    requested elsewhere it uses the cached module. So working with parent module filename cozes the
    first module's dirname to used instead of the actual requiring module dirname. Therefore when the
    workaround is to specify the dirname when booting the hook for the specific micro app hook. See the
    change.

--- test/sails-hook-before-orm/index.js
+++ test/sails-hook-before-orm/index.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = function (sails) {
-    var loader = require('../../')(sails);
+    var loader = require('../../')(sails, __dirname);
 
     // Load policies and config from default directories
     loader.configure();
