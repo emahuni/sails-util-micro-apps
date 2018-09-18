e5a8eeca4a553ee8f0bb732af0cd14e58792d89c
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-18 19:35:24 +0200

    test(app): add app api routes injection control tests

--- test/fixtures/app/config/routes.js
+++ test/fixtures/app/config/routes.js
@@ -11,32 +11,33 @@
 module.exports.routes = {
 
 
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+				/***************************************************************************
+					*                                                                          *
+					* More custom routes here...                                               *
+					* (See https://sailsjs.com/config/routes for examples.)                    *
+					*                                                                          *
+					* If a request to a URL doesn't match any of the routes in this file, it   *
+					* is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+					* not match any of those, it is matched against static assets.             *
+					*                                                                          *
+					***************************************************************************/
 
 
-  //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
-  //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
-  //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
+				//  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
+				//  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
+				//  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
+    '/app/ctrltest': 'AppController.appCtrlTest',
+    '/app/actiontest': 'appaction/test-action',
 
 
+				//  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
+				//  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
+				//  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
 
-  //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
-  //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
-  //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
 
-
-  //  ╔╦╗╦╔═╗╔═╗
-  //  ║║║║╚═╗║
-  //  ╩ ╩╩╚═╝╚═╝
+				//  ╔╦╗╦╔═╗╔═╗
+				//  ║║║║╚═╗║
+				//  ╩ ╩╩╚═╝╚═╝
 
 
 };
