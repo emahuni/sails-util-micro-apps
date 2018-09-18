182badd1f9fd515ba8116d129f8bdf51d2ba3789
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-18 14:52:44 +0200

    test(app): add app api testMicroApp entry

--- test/all.test.js
+++ test/all.test.js
@@ -9,26 +9,32 @@ describe('Sails-hook-micro-apps Hook tests #', function () {
         return true;
     });
 
+    // this tests the app api that we know was loaded normally, nothing should fail otherwise we have broken tests in testMicroApp
+    context(`App API control test - none under here should fail, otherwise the test is broken::`, async function () {
+						  testMicroApp('makes sure that the tests are working as expected', 'app');
+				});
+
+    // now test the injections
     context('Models Injection ::', async function () {
-				context(`Before ORM Hook - Micro App injection ::`, async function () {
-						testMicroApp('Injected using the parent module path', 'before');
-				});
+				    context(`Before ORM Hook - Micro App injection ::`, async function () {
+						      testMicroApp('Injected using the parent module path', 'before');
+				    });
 
-				context(`After ORM Hook - Micro App injection ::`, async function () {
-						testMicroApp('Injected using a path given to configure and adapt in the requiring module', 'after');
-				});
+				    context(`After ORM Hook - Micro App injection ::`, async function () {
+						      testMicroApp('Injected using a path given to configure and adapt in the requiring module', 'after');
+				    });
 
-				context(`Runtime Micro App injection ::`, async function () {
-						before(async function (){
-								const loader = require('../')(sails, __dirname + '/fixtures/runtime-micro-app');
+				    context(`Runtime Micro App injection ::`, async function () {
+						      before(async function (){
+								        const loader = require('../')(sails, __dirname + '/fixtures/runtime-micro-app');
 
-								loader.configure();
+								        loader.configure();
 
-								loader.adapt();
-						});
+								        loader.adapt();
+						      });
 
-						testMicroApp(`Injected by using given loader path)`, 'runtime');
-				});
+						      testMicroApp(`Injected by using given loader path)`, 'runtime');
+				    });
     });
 
 });
