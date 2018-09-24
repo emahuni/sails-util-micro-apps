336734a48295d93dbea70104c4b942c7f8c04cc9
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-24 20:24:19 +0200

    refactor(libs/policy): adopt policy changes

--- libs/sails/_loadPolicies.js
+++ libs/sails/_loadPolicies.js
@@ -4,21 +4,21 @@
  * @param {Object} options
  * @param {Function} cb
  */
-var async = require('async'), _ = require('lodash'), buildDictionary = require('sails-build-dictionary');
+const async = require('async')
+const _ = require('lodash')
+const buildDictionary = require('sails-build-dictionary')
 
 module.exports = function (cb) {
-    async.reduce(sails.config.paths.policies, {}, function (prev, curr, callback) {
-        buildDictionary.optional({
-            dirname: curr,
-            filter: /(.+)\.(js|coffee|litcoffee)$/,
-            replaceExpr: null,
-            flattenDirectories: true,
-            keepDirectoryPath: true
-        }, function (err, policies) {
-            if (err) {
-                callback(err);
-            }
-            callback(null, _.merge(prev, policies));
-        });
-    }, cb);
-};
+  async.reduce(sails.config.paths.policies, {}, function (prev, curr, callback) {
+    buildDictionary.optional({
+      dirname: curr,
+      filter: /(.+)\.(js|coffee|litcoffee)$/,
+      replaceExpr: null,
+      flattenDirectories: true,
+      keepDirectoryPath: true
+    }, (err, policies) => {
+      if (err) callback(err)
+      callback(null, _.merge(prev, policies))
+    })
+  }, cb)
+}
