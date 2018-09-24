3f71259035792287c3b20850481e08712b004234
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-24 07:48:35 +0200

    test(policy): add runtime policies

--- test/fixtures/runtime-micro-app/config/policies.js
+++ test/fixtures/runtime-micro-app/config/policies.js
@@ -1,6 +1,30 @@
 /**
- * Created by Emmanuel Mahuni. MIT 2018
+ * Policy Mappings
+ * (sails.config.policies)
+ *
+ * Policies are simple functions which run **before** your actions.
+ *
+ * For more information on configuring policies, check out:
+ * https://sailsjs.com/docs/concepts/policies
  */
+
 module.exports.policies = {
-//    '*': ['runtime-ctrl-test']
+				/***************************************************************************
+					*                                                                          *
+					* Default policy for all controllers and actions, unless overridden.       *
+					* (`true` allows public access)                                            *
+					*                                                                          *
+					***************************************************************************/
+
+				// '*': true,
+				RuntimeController:{
+								// test controller allowing
+								'runtimeCtrlPolicyAllow': 'allowRuntime',
+								// test controller denying
+								'runtimeCtrlPolicyDeny': 'denyRuntime',
+				},
+
+				// standalone actions
+				'runtimeaction/act-policy-allow': 'allowRuntime',
+				'runtimeaction/act-policy-deny': 'denyRuntime',
 };
