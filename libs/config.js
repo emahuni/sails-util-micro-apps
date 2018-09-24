607124cdd87691751ae583b2f5847dce9d87c955
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-24 09:55:00 +0200

    refactor(config): adopt config changes

--- libs/config.js
+++ libs/config.js
@@ -2,20 +2,24 @@
  * Load config from a directory into a Sails app
  */
 
-var buildDictionary = require('sails-build-dictionary');
-var _ = require('lodash');
+const includeAll = require('include-all');
+const _ = require('lodash');
+
+
 module.exports = function (sails, dir) {
-    buildDictionary.aggregate({
-        dirname: dir,
-        exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
-        excludeDirs: /(locales|env)$/,
-        filter: /(.+)\.(js|json|coffee|litcoffee)$/,
-        identity: false
-    }, function (err, configs) {
-        sails.config = _.merge(configs, sails.config, function (a, b) {
-            if (_.isArray(a)) {
-                return a.concat(b);
-            }
-        });
-    });
+				includeAll.optional({
+								dirname: dir,
+								exclude: ['locales', 'local.js', 'local.json', 'local.coffee', 'local.litcoffee'],
+								excludeDirs: /(locales|env)$/,
+								filter: /(.+)\.(js|json|coffee|litcoffee)$/,
+								identity: false
+				}, function (err, configs) {
+								if (err) sails.log.error(err);
+								sails.config = _.merge(configs, sails.config, (a, b) => _.isArray(a) ? a.concat(b) : undefined);
+
+								// Using this hack to reset and bind our policies to router
+								sails._actionMiddleware = [];
+								sails.router.flush();
+				});
 };
+
